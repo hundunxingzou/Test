4104beffb46bd2c3dce5d6e4b957d7abfdc8e53c
hundunxingzou <109846482+hundunxingzou@users.noreply.github.com>
2023-02-21 10:25:55 +0800

    Update config.js

--- src/chains/providers/config.js
+++ src/chains/providers/config.js
@@ -1,4 +1,4 @@
-const key = ['7022eb08e0ec7f44574cf96ed1f00fe39b203972e9abd5c776dfd26ecb06a912','65cbaa8552f17310df9a47a9fdc1d07a77aff72d1608ffa45c6a5eebea3ffde0','cea42d8abb848f006de81fb2ce745731edae8e4cf70c5b48d38379f6c4c26bfb']
+const key = ['','','']
 const bscurl='http://bsc.mezz.fi/'
 // 线性曲线
 const linearCurve_address='0xc392A78DE7eca339f135b39557c7CaCa9fe814cc';
